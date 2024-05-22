0c17a47c2ebc203db2be19293606b8f8e25fc867
imoken777 <s1f102301640@iniad.org>
2024-05-22 20:08:02 +0900

    feat: Add checkbox functionality for prefectures

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,17 @@ import { useEffect, useState } from 'react';
 const Home = () => {
   const [prefectures, setPrefectures] = useState<Prefecture[]>([]);
 
+  const handlePrefectureCheckbox = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, checked } = event.target;
+    const newPrefectures = prefectures.map((prefecture) => {
+      if (prefecture.prefName === name) {
+        return { ...prefecture, isChecked: checked };
+      }
+      return prefecture;
+    });
+    setPrefectures(newPrefectures);
+  };
+
   const fetchPrefectures = async () => {
     const url = 'https://opendata.resas-portal.go.jp/api/v1/prefectures';
     const headers = {
@@ -28,6 +39,35 @@ const Home = () => {
   useEffect(() => {
     fetchPrefectures();
   }, []);
+  return (
+    <div className={styles.container}>
+      {prefectures.length > 0 && (
+        <div className={styles.prefecturesContainer}>
+          {prefectures.map((prefecture) => (
+            <div key={prefecture.prefCode}>
+              <label>
+                <input
+                  type="checkbox"
+                  name={prefecture.prefName}
+                  onChange={handlePrefectureCheckbox}
+                />
+                {prefecture.prefName}
+              </label>
+            </div>
+          ))}
+        </div>
+      )}
+
+      <h1>checked Items</h1>
+      <ul>
+        {prefectures
+          .filter((prefecture) => prefecture.isChecked)
+          .map((prefecture) => (
+            <li key={prefecture.prefCode}>{prefecture.prefName}</li>
+          ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
